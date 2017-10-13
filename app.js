df49c50b95f405e9d1fff281d0f3f5b1a9bd889a
root <root@blazy-UX305UAB>
2017-10-13 23:17:51 +0200

    tp4 modifs

--- app.js
+++ app.js
@@ -21,67 +21,8 @@ server.post('/api/messages', connector.listen());
 // This bot ensures user's profile is up to date.
 var bot = new botbuilder.UniversalBot(connector, [
     function (session) {
-        //session.beginDialog('ensureProfile', session.userData.profile);
-        //session.beginDialog('reservation:reservationHostel');
-        //session.beginDialog('alarm:createAlarm');
-        session.send("Welcome to your alarm");
+        session.send("Welcome to your alarm bot");
         session.beginDialog("mainMenu");
-    }/*,
-    function (session, results) {
-        session.userData.profile = results.response; // Save user profile.
-        session.send(`Hello ${session.userData.profile.name}! I love ${session.userData.profile.company}!`);
-    }*/
-]);
-
-bot.library(reservation);
-
-/*bot.dialog('ensureProfile', [
-    function (session, args, next) {
-        session.dialogData.profile = args || {}; // Set the profile or create the object.
-        if (!session.dialogData.profile.name) {
-            botbuilder.Prompts.text(session, "What's your name?");
-        } else {
-            next(); // Skip if we already have this info.
-        }
-    },
-    function (session, results, next) {
-        if (results.response) {
-            // Save user's name if we asked for it.
-            session.dialogData.profile.name = results.response;
-        }
-        if (!session.dialogData.profile.company) {
-            botbuilder.Prompts.text(session, "What company do you work for?");
-        } else {
-            next(); // Skip if we already have this info.
-        }
-    },
-    function (session, results) {
-        if (results.response) {
-            // Save company name if we asked for it.
-            session.dialogData.profile.company = results.response;
-        }
-        session.endDialogWithResult({ response: session.dialogData.profile });
-    }
-]);*/
-
-bot.dialog('dinnerOrder', [
-    (session) => {
-        botbuilder.Prompts.text(session, "Hello... What's your name?");
-    },
-    (session, results) => {
-        session.userData.name = results.response;
-        botbuilder.Prompts.time(session, "Hi " + results.response + ", when do you want to make a reservation ?");
-    },
-    (session, results) => {
-        session.userData.coding = results.response;
-        botbuilder.Prompts.text(session, "Hello... What's your name?");
-        //botbuilder.Prompts.choice(session, "What language do you code Node using?", ["JavaScript", "CoffeeScript", "TypeScript"]);
-    },
-    (session, results) => {
-        session.userData.language = results.response.entity;
-        session.send("Got it... " + session.userData.name +
-            " you've been programming for " + session.userData.coding +
-            " years and use " + session.userData.language + ".");
     }
 ]);
 
